4d51461623069830a1c88951217aab2e5e1fa172
Epsit <99855060+epsit03@users.noreply.github.com>
2024-10-09 03:27:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const validUsername = "admin";
         // document.getElementsByClassName('bleh').style.display = 'flex';
         var elements = document.getElementsByClassName('bleh');
 for (var i = 0; i < elements.length; i++) {
-  elements[i].style.display = 'flex';
+  elements[i].style.display = 'flexbox';
 }
 
         document.querySelector('table').style.display = 'table';
